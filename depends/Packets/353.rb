1b3868c8713da5048c70167ca8312179d0a382d3
umby24 <wanderingscripter@gmail.com>
2014-01-26 13:07:01 -0600

    Fixed multiple $authed entries

--- depends/Packets/353.rb
+++ depends/Packets/353.rb
@@ -1,17 +1,21 @@
 $arr = $message.split(" ",120)
 $players[$splits[3]] = $arr
 $arr.each do |f|
-    if f.include?("~") || f.include?("@") || f.include?("+")
-        f = f.gsub("~","")
-        f = f.gsub("@","")
-        f = f.gsub("+","")
-        f = f.gsub(" ","")
-        $authed[$authed.length] = f
-        next
-    end
-    f = f.gsub("~","")
-    f = f.gsub("@","")
-    f = f.gsub("+","")
-    f = f.gsub(" ","")
-    send_raw("whois #{f}")
+	if f.include?("~") || f.include?("@") || f.include?("+")
+		f = f.gsub("~","")
+		f = f.gsub("@","")
+		f = f.gsub("+","")
+		f = f.gsub(" ","")
+		
+		if $authed.include?(f) == false
+			$authed[$authed.length] = f
+		end
+		
+		next
+	end
+	f = f.gsub("~","")
+	f = f.gsub("@","")
+	f = f.gsub("+","")
+	f = f.gsub(" ","")
+	send_raw("whois #{f}")
 end
